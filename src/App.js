bb8da0b4d0486eff10083f7098e5ba2178a01a48
Lucas <lucaslapp9@gmail.com>
2021-03-23 10:52:28 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import $ from 'jquery';
 
 function App() {
+  const [starships, setStarships] = useState([]);
+  const [newStarships, setNewStarships] = useState([]);
+  const [distance, setDistance] = useState('');
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  const apiCall = () => {
+    $.ajax({
+      url: 'https://swapi.dev/api/starships/',
+      method: 'get',
+      dataType: 'json',
+      success: (ret) => {
+        let arrayStarships = [];
+        for (let starship of ret.results) {
+          arrayStarships.push({ name: starship.name, hours: convertHours(starship.consumables), mglt: starship.MGLT });
+        }
+        setStarships(arrayStarships);
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  }
+
+  const stopsCalculator = (e) => {
+    e.preventDefault();
+    let arrayStarships = [];
+    for (let starship of starships) {
+      let paradas = (distance / (starship.hours * starship.mglt));
+      arrayStarships.push({ name: starship.name, stops: parseInt(paradas) })
+    }
+    setNewStarships(arrayStarships);
+    setDistance("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">Calculadora Star Wars</div>
+      <div className="main">
+        <p>Este Programa calcula quantas paradas são nescessárias para uma nave de Star Wars percorrer uma determinada distância.</p>
+        <form onSubmit={e => stopsCalculator(e)}>
+          <label htmlFor="distance">Digite uma distância:</label>
+          <input type="number" value={distance} name="distance" id="distance" required onChange={(e) => setDistance(e.target.value)}/>
+          <button type="submit">Calcular</button>
+          <button type="button" onClick={e => setNewStarships([])}>Limpar</button>
+        </form>
+          {newStarships.map(starship =>
+            <div key={starship.name}>{starship.name}: {starship.stops}</div>
+          )}
+      </div>
+    </>
   );
 }
 
 export default App;
+
+const convertHours = (consumables) => {
+  let hours = 0;
+  if (consumables.includes('year')) {
+    hours = parseInt(consumables) * 8760;
+  } else if (consumables.includes('month')) {
+    hours = parseInt(consumables) * 730;
+  } else if (consumables.includes('week')) {
+    hours = parseInt(consumables) * 168;
+  } else if (consumables.includes('day')) {
+    hours = parseInt(consumables) * 24;
+  }
+  return hours;
+}
+
+
